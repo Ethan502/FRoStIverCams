d3fd77f46f831f9fac36a2fbb326955ded66d9b7
Ethan <esmit502@byu.edu>
2022-06-27 15:20:24 -0600

    started new branch

--- recorder.py
+++ recorder.py
@@ -1,6 +1,10 @@
 import cv2
 from vimba import *
 
+cam1_ID = "DEV_000F314F3265"
+cam2_ID = "DEV_000F314F3266"
+
+
 def setup_camera(cam: Camera):
     with cam:
         # Enable auto exposure time setting if camera supports it
@@ -29,14 +33,25 @@ def setup_camera(cam: Camera):
 
 with Vimba.get_instance() as vimba:
     cams = vimba.get_all_cameras()
-    with cams[0] as cam:
+    for cam in cams:
         setup_camera(cam)
-        while(1):
+    while(1):
+        for cam in cams:
             frame = cam.get_frame()
             frame.convert_pixel_format(PixelFormat.Bgr8)
-            #cv2.imshow('frame', frame.as_opencv_image())
-            filename = f"images/image{frame.get_id}.jpg"
-            cv2.imwrite(filename,frame.as_opencv_image())
+            if(cam.get_id() == cam1_ID):
+                filename = f"images1/image{frame.get_id()}.jpg"
+            elif(cam.get_id() == cam2_ID):
+                filename = f"images2/image{frame.get_id()}.jpg"
+            cv2.imwrite(filename, frame.as_opencv_image())
 
-            if cv2.waitKey(1) == ord('q'):
-                break
+
+        # while(1):
+        #     frame = cam.get_frame()
+        #     frame.convert_pixel_format(PixelFormat.Bgr8)
+        #     #cv2.imshow('frame', frame.as_opencv_image())
+        #     filename = f"images/image{frame.get_id}.jpg"
+        #     cv2.imwrite(filename,frame.as_opencv_image())
+
+        #     if cv2.waitKey(1) == ord('q'):
+        #         break
