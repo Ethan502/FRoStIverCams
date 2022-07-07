1765d3bc4b3d4c5b0d889e894975a81c374a587a
Ethan <esmit502@byu.edu>
2022-07-07 12:44:14 -0600

    updates

--- recorder.py
+++ recorder.py
@@ -31,27 +31,36 @@ def setup_camera(cam: Camera):
         except (AttributeError, VimbaFeatureError):
             pass
 
+def grabber(cam):
+    frame = cam.get_frame()
+    frame.convert_pixel_format(PixelFormat.Bgr8)
+    filename = f"images1/image{frame.get_id}.jpg"
+    cv2.imwrite(filename,frame.as_opencv_image())
+
 with Vimba.get_instance() as vimba:
     cams = vimba.get_all_cameras()
     for cam in cams:
         setup_camera(cam)
+   
     while(1):
-        for cam in cams:
-            frame = cam.get_frame()
-            frame.convert_pixel_format(PixelFormat.Bgr8)
-            if(cam.get_id() == cam1_ID):
-                filename = f"images1/image{frame.get_id()}.jpg"
-            elif(cam.get_id() == cam2_ID):
-                filename = f"images2/image{frame.get_id()}.jpg"
-            cv2.imwrite(filename, frame.as_opencv_image())
+        frame = cam.get_frame()
+        frame.convert_pixel_format(PixelFormat.Bgr8)
+        #cv2.imshow('frame', frame.as_opencv_image())
+        filename = f"images/image{frame.get_id}.jpg"
+        cv2.imwrite(filename,frame.as_opencv_image())
 
+        if cv2.waitKey(1) == ord('q'):
+            break
 
-        # while(1):
-        #     frame = cam.get_frame()
-        #     frame.convert_pixel_format(PixelFormat.Bgr8)
-        #     #cv2.imshow('frame', frame.as_opencv_image())
-        #     filename = f"images/image{frame.get_id}.jpg"
-        #     cv2.imwrite(filename,frame.as_opencv_image())
+    # while(1):
+    #     for cam in cams:
+    #         frame = cam.get_frame()
+    #         frame.convert_pixel_format(PixelFormat.Bgr8)
+    #         if(cam.get_id() == cam1_ID):
+    #             filename = f"images1/image{frame.get_id()}.jpg"
+    #         elif(cam.get_id() == cam2_ID):
+    #             filename = f"images2/image{frame.get_id()}.jpg"
+    #         cv2.imwrite(filename, frame.as_opencv_image())
 
-        #     if cv2.waitKey(1) == ord('q'):
-        #         break
+    
+    
