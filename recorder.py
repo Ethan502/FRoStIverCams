8a29b5d3b4923822a7cb9763c98cf96d82ffb30d
Ethan <esmit502@byu.edu>
2022-07-08 12:59:58 -0600

    cameras can multithread photos now

--- recorder.py
+++ recorder.py
@@ -5,6 +5,7 @@ import threading
 
 cam1_ID = "DEV_000F314F3265"
 cam2_ID = "DEV_000F314F3266"
+pic_count = 0
 
 
 def setup_camera(cam: Camera):
@@ -34,41 +35,55 @@ def setup_camera(cam: Camera):
         except (AttributeError, VimbaFeatureError):
             pass
 
-def grabber(cam):
-    frame = cam.get_frame()
-    frame.convert_pixel_format(PixelFormat.Bgr8)
-    filename = f"images1/image{frame.get_id}.jpg"
-    cv2.imwrite(filename,frame.as_opencv_image())
+def grabber(cam, count):
+    with cam as c:            
+        frame=c.get_frame()
+        frame.convert_pixel_format(PixelFormat.Bgr8)
+        if(c.get_id() == cam1_ID):
+            print("Ran1")
+            filename = f"images1/image{count}.jpg"
+        elif(c.get_id() == cam2_ID):
+            print("Ran2")
+            filename = f"images2/image{count}.jpg"
+
+        cv2.imwrite(filename, frame.as_opencv_image())
+        count += 1
 
 with Vimba.get_instance() as vimba:
     cams = vimba.get_all_cameras()
 
     #threads the setup camera function
-    threads = []
+    setup_threads = []
     for cam in cams:
         t = threading.Thread(target=setup_camera, args=(cam,))
         t.start()
-        threads.append(t)
-    for thread in threads:
+        setup_threads.append(t)
+    for thread in setup_threads:
         thread.join()
 
-    for cam in cams:
-#___________________________________________________________________________ thread this section
-        with cam as c:
-            count = 0
-            while(count < 5):
-                frame=cam.get_frame()
-                frame.convert_pixel_format(PixelFormat.Bgr8)
-                if(cam.get_id() == cam1_ID):
-                    print("Ran1")
-                    filename = f"images1/image{count}.jpg"
-                elif(cam.get_id() == cam2_ID):
-                    print("Ran2")
-                    filename = f"images2/image{count}.jpg"
+    start = time.perf_counter()
+   
+    camera_threads = []
+    pic_index = 0
+    try:
+        while(1):
+            for cam in cams:
+                t = threading.Thread(target=grabber, args=(cam,pic_index))
+                t.start()
+                camera_threads.append(t)
+                time.sleep(0.10) #this sleep is to give time for the delay in the switch
+            pic_index += 1
 
-                cv2.imwrite(filename, frame.as_opencv_image())
-                count += 1
-#__________________________________________________________________________
+            for thread in camera_threads:
+                thread.join()
 
-    print(f"Finished cam setup in {finish - start} seconds")
+            if cv2.waitKey == ord('q'):
+                break
+    except KeyboardInterrupt:
+        pass
+
+    
+        
+    finish = time.perf_counter()
+    print(f"Finished grabbing 5 pics on 2 cams in {finish - start} seconds")
     
